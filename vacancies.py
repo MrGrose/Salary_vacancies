c9017efe617c16da9392908fe3204e5a39ee5ef4
MrGrose <grachev.ro@gmail.com>
2025-01-11 14:53:22 +0300

    Fixed it to separate functions for each job site

--- vacancies.py
+++ vacancies.py
@@ -3,60 +3,76 @@ import time
 import requests
 
 
-def fetch_vacancies(url, headers=None, params=None, page=0, pages_number=1):
-    vacancies = []
-    while page < pages_number:
-        params["page"] = page
-        try:
-            response = requests.get(url, headers=headers, params=params)
-            response.raise_for_status()
-            payload = response.json()
-            if "pages" in payload:
-                pages_number = payload["pages"]
-            elif "total" in payload:
-                pages_number = payload["total"] // params["count"]
-                if pages_number * params["count"] < payload["total"]:
-                    pages_number += 1
-            page += 1
-            
-            if "items" in payload:
-                vacancies.extend(payload["items"])
-            elif "objects" in payload:
-                vacancies.extend(payload["objects"])
-            time.sleep(1)
-        except requests.exceptions.RequestException as e:
-            print(f"Ошибка при запросе: {e}")
-            break
-    return vacancies
-
-
 def get_vacancies_hh(languages):
-    url = "https://api.hh.ru/vacancies"
-    town_id = 1
-    params = {
-        "area": town_id,
-        "search_field": "name",
-    }
     vacancies = {}
+    town_id = 1
     for language in languages:
-        params["text"] = language
-        vacancies[language] = fetch_vacancies(url, params=params)
+        vacancies[language] = {"count": 0, "items": []}
+        pages_number = 1
+        page = 0
+        while page < pages_number:
+            params = {
+                "area": town_id,
+                "text": language,
+                "search_field": "name",
+                "page": page
+            }
+            try:
+                page_response = requests.get(
+                    "https://api.hh.ru/vacancies",
+                    params=params,
+                )
+                page_response.raise_for_status()
+                page_payload = page_response.json()
+                pages_number = page_payload["pages"]
+                page += 1
+                vacancies[language]["count"] = page_payload["found"]
+                vacancies[language]["items"].extend(page_payload["items"])
+                time.sleep(0.5)
+            except requests.exceptions.RequestException as e:
+                print(f"Ошибка при запросе: {e}")
+                break
     return vacancies
 
 
 def get_vacancy_superjob(languages, superjob_key):
-    url = "https://api.superjob.ru/2.0/vacancies/"
     headers = {"X-Api-App-Id": superjob_key}
+    vacancies = {}
     town_id = 4
     profession_id = 48
     number_vacancies_on_page = 20
-    params = {
-        "town": town_id,
-        "catalogues": profession_id,
-        "count": number_vacancies_on_page,
-    }
-    vacancies = {}
+    vacancies_number_limit = 500
     for language in languages:
-        params["keyword"] = language
-        vacancies[language] = fetch_vacancies(url, headers=headers, params=params)
+        vacancies[language] = {"count": 0, "items": []}
+        pages_number = 1
+        page = 0
+        while page < pages_number:
+            params = {
+                "town": town_id,
+                "catalogues": profession_id,
+                "keyword": language,
+                "page": page,
+                "count": number_vacancies_on_page
+            }
+            try:
+                page_response = requests.get(
+                    "https://api.superjob.ru/2.0/vacancies/",
+                    headers=headers,
+                    params=params,
+                )
+                page_response.raise_for_status()
+                page_payload = page_response.json()
+                vacancies[language]["count"] = page_payload["total"]
+                if page_payload["total"] > vacancies_number_limit:
+                    pages_number = vacancies_number_limit // number_vacancies_on_page
+                elif page_payload["total"] < number_vacancies_on_page:
+                    pages_number = 1
+                else:
+                    pages_number = page_payload["total"] // number_vacancies_on_page
+                page += 1
+                vacancies[language]["items"].extend(page_payload["objects"])
+                time.sleep(0.5)
+            except requests.exceptions.RequestException as e:
+                print(f"Ошибка при запросе: {e}")
+                break
     return vacancies
